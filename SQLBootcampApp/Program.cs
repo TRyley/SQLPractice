c4e17b18c25b47f0d3656718ff6d0df57bc849bc
Terry Ryley <terry_ryley@waters.com>
2017-06-13 15:59:49 +0100

    initial commit

---
+++ SQLBootcampApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SQLBootcampApp
+{
+    class Program
+    {
+        private static string myConnectionString = "server=localhost;database=battlerecord;uid=root;pwd=KR19tr222013;";
+        static void Main(string[] args)
+        {
+            MySqlConnection cnn;
+            cnn = new MySqlConnection(myConnectionString);
+            try
+            {
+                cnn.Open();
+                Console.WriteLine("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not open connection ! ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
